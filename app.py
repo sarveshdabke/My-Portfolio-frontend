c2b87dd540ed39c1149e631886947781d31fd92d
sarveshdabke <vaultlocker.official@gmail.com>
2025-11-20 16:55:28 +0530

    Updated backend and frontend changes

--- app.py
+++ app.py
@@ -157,6 +157,8 @@ def contact():
     except Exception as e:
         print(f"Email Error: {e}")
         return jsonify({"error": "Failed to send message."}), 500
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
